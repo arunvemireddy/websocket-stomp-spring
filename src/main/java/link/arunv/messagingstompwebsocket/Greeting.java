47b0a9cabf9e6b95e8e7dfe4b5e3fcfea21528d5
arunvemireddy <vemireddyarun0701@gmail.com>
2024-01-28 18:59:56 -0700

    websocket-stomp

---
+++ src/main/java/link/arunv/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,18 @@
+package link.arunv.messagingstompwebsocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
